d091303c50992b06d6a7bfdce44829ec190f0ad2
kiran.sunkari <sunkarikiran1580@gmail.com>
2023-09-23 19:44:43 +0530

    some imp shell shiripts

---
+++ ifelse.sh
@@ -0,0 +1,25 @@
+##if else loop exmaple
+#
+#if [express]
+#then
+#   ...
+#   .....
+#   ,,,,,,
+#else
+#   '''''
+#   ....
+#fi   
+a=4
+b=10
+if[$a > $b]
+then
+	echo "A IS GREATHER THEN B"
+else
+	echo "B is graether then a"
+fi
+### for loop, to execute the same actions continously..
+#iterations  ---> for
+#example to print numbers 1 to 10
+#while, for, until,select loop....
+#for i in {1.100}; do echo $1; done
+
---
+++ nodehelath.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#######################
+#Kiran
+#date:9-23-2023
+#
+#This Script output the node health
+#
+#Version: v1
+###########################
+#
+
+set -x # debug mode
+set -e # exit the script when their is error
+set -o # pipefile
+df -h
+
+
+
+date
+
+nproc
